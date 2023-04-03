b114138ac8cf085c91dcbf9a4d90103e6009834a
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2023-04-03 20:20:51 -0300

    fix filePath

--- src/Test.java
+++ src/Test.java
@@ -6,23 +6,23 @@ import lexical.Tag;
 import lexical.Token;
 
 public class Test {
-	public static void main(String[] args) throws Exception {
-        String filePath = "./programs/teste5";
-        
+    public static void main(String[] args) throws Exception {
+        String filePath = "../programs/teste4";
+
         Lexer lexer = new Lexer(filePath);
         Token token;
-        
-		// run scan from init file to EOF
+
+        // run scan from init file to EOF
         do {
             token = lexer.scan();
             System.out.println(token);
         } while (token.getTag() != Tag.END_OF_FILE);
 
-		// debug symbolTable
-		// for (Entry<String, Tag> entry : lexer.words.table.entrySet()) {
-        //     String key = entry.getKey();
-        //     Tag value = entry.getValue();
-        //     System.out.println("Chave: " + key + ", Valor: " + value);
+        // debug symbolTable
+        // for (Entry<String, Tag> entry : lexer.words.table.entrySet()) {
+        // String key = entry.getKey();
+        // Tag value = entry.getValue();
+        // System.out.println("Chave: " + key + ", Valor: " + value);
         // }
     }
 }
