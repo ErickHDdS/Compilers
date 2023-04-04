0c712d8d047501c1b8cb399f6c18219acd5c5720
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2023-04-03 22:17:48 -0300

    optimize test

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,6 @@
+import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Map.Entry;
 
 import lexical.Lexer;
@@ -7,22 +9,36 @@ import lexical.Token;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        String filePath = "../programs/teste4";
+        Scanner input = new Scanner(System.in);
 
-        Lexer lexer = new Lexer(filePath);
-        Token token;
+        String fileName;
 
-        // run scan from init file to EOF
-        do {
-            token = lexer.scan();
-            System.out.println(token);
-        } while (token.getTag() != Tag.END_OF_FILE);
+        while (true) {
+            System.out.println("\nDigite o nome do programa: \nCRTL + C para encerrar.\n");
+            fileName = input.nextLine();
+            String filePath;
+            Lexer lexer;
+            Token token;
 
-        // debug symbolTable
-        // for (Entry<String, Tag> entry : lexer.words.table.entrySet()) {
-        // String key = entry.getKey();
-        // Tag value = entry.getValue();
-        // System.out.println("Chave: " + key + ", Valor: " + value);
-        // }
+            try {
+                filePath = "../programs/" + fileName;
+                lexer = new Lexer(filePath);
+                // run scan from init file to EOF
+                do {
+                    token = lexer.scan();
+                    System.out.println(token.toString() + " ");
+                } while (token.getTag() != Tag.END_OF_FILE);
+
+                // debug symbolTable
+                // for (Entry<String, Tag> entry : lexer.words.table.entrySet()) {
+                // String key = entry.getKey();
+                // Tag value = entry.getValue();
+                // System.out.println("Chave: " + key + ", Valor: " + value);
+                // }
+            } catch (FileNotFoundException e) {
+                System.out.println("Insira o nome de um arquivo válido.");
+            }
+
+        }
     }
 }
