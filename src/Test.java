1a62d54dabc327ca2f971dd801ecf03c4bb486b8
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2023-04-11 23:46:22 -0300

    adjusting the fileName

--- src/Test.java
+++ src/Test.java
@@ -9,30 +9,29 @@ public class Test {
     public static void main(String[] args) throws Exception {
         // Scanner input = new Scanner(System.in);
 
-        String fileName = args[0];
+        String fileName = "../programs/" + args[0];
 
-            Lexer lexer;
-            Token token;
+        Lexer lexer;
+        Token token;
 
-            try {
-                lexer = new Lexer(fileName);
-                do {
-                    token = lexer.scan();
-                    System.out.println(token.toString() + " ");
-                } while (token.getTag() != Tag.END_OF_FILE);
+        try {
+            lexer = new Lexer(fileName);
+            do {
+                token = lexer.scan();
+                System.out.println(token.toString() + " ");
+            } while (token.getTag() != Tag.END_OF_FILE);
 
-                // debug symbolTable
-                // for (Entry<String, Tag> entry : lexer.words.table.entrySet()) {
-                // String key = entry.getKey();
-                // Tag value = entry.getValue();
-                // System.out.println("Chave: " + key + ", Valor: " + value);
-                // }
-            } catch (FileNotFoundException e) {
-                System.out.println("Insira o nome de um arquivo válido.");
-            }
-            catch (CompilerException e) {
-                System.out.println(e);
-            }
+            // debug symbolTable
+            // for (Entry<String, Tag> entry : lexer.words.table.entrySet()) {
+            // String key = entry.getKey();
+            // Tag value = entry.getValue();
+            // System.out.println("Chave: " + key + ", Valor: " + value);
+            // }
+        } catch (FileNotFoundException e) {
+            System.out.println("Insira o nome de um arquivo válido.");
+        } catch (CompilerException e) {
+            System.out.println(e);
+        }
 
     }
 }
